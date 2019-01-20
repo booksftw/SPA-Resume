6f6c22c4297378724466ba9b36f1617e0fe4c0a1
Nicholas Zachary <nick@Nicholass-MacBook-Air.local>
2019-01-20 15:10:58 -0800

    media query intro font text complete

--- src/app/resume-professional/resume-professional.component.ts
+++ src/app/resume-professional/resume-professional.component.ts
@@ -7,9 +7,87 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ResumeProfessionalComponent implements OnInit {
 
+  projects;
+
   constructor() { }
 
   ngOnInit() {
+
+    this.projects = [];
+
+    this.projects.push(
+      {
+        title: 'Dota2Edge',
+        imgsrc: '/assets/images/dota2-wallpaper.jpg',
+        description: 'Angular app that intelligently figures out to pick the best dota2 heroes'
+      },
+      {
+        title: 'Dota2Edge',
+        imgsrc: '/assets/images/dota2-wallpaper.jpg',
+        description: 'Angular app that intelligently figures out to pick the best dota2 heroes'
+      },
+      {
+        title: 'Dota2Edge',
+        imgsrc: '/assets/images/dota2-wallpaper.jpg',
+        description: 'Angular app that intelligently figures out to pick the best dota2 heroes'
+      },
+      {
+        title: 'Dota2Edge',
+        imgsrc: '/assets/images/dota2-wallpaper.jpg',
+        description: 'Angular app that intelligently figures out to pick the best dota2 heroes'
+      },
+      {
+        title: 'Dota2Edge',
+        imgsrc: '/assets/images/dota2-wallpaper.jpg',
+        description: 'Angular app that intelligently figures out to pick the best dota2 heroes'
+      },
+      {
+        title: 'Dota2Edge',
+        imgsrc: '/assets/images/dota2-wallpaper.jpg',
+        description: 'Angular app that intelligently figures out to pick the best dota2 heroes'
+      },
+      {
+        title: 'Dota2Edge',
+        imgsrc: '/assets/images/dota2-wallpaper.jpg',
+        description: 'Angular app that intelligently figures out to pick the best dota2 heroes'
+      },
+      {
+        title: 'Dota2Edge',
+        imgsrc: '/assets/images/dota2-wallpaper.jpg',
+        description: 'Angular app that intelligently figures out to pick the best dota2 heroes'
+      },
+      {
+        title: 'Dota2Edge',
+        imgsrc: '/assets/images/dota2-wallpaper.jpg',
+        description: 'Angular app that intelligently figures out to pick the best dota2 heroes'
+      },
+      {
+        title: 'Dota2Edge',
+        imgsrc: '/assets/images/dota2-wallpaper.jpg',
+        description: 'Angular app that intelligently figures out to pick the best dota2 heroes'
+      },
+      {
+        title: 'Dota2Edge',
+        imgsrc: '/assets/images/dota2-wallpaper.jpg',
+        description: 'Angular app that intelligently figures out to pick the best dota2 heroes'
+      },
+      {
+        title: 'Dota2Edge',
+        imgsrc: '/assets/images/dota2-wallpaper.jpg',
+        description: 'Angular app that intelligently figures out to pick the best dota2 heroes'
+      },
+      {
+        title: 'Dota2Edge',
+        imgsrc: '/assets/images/dota2-wallpaper.jpg',
+        description: 'Angular app that intelligently figures out to pick the best dota2 heroes'
+      },
+      {
+        title: '',
+        imgsrc: '',
+        description: ''
+      },
+    );
+
   }
 
 }
